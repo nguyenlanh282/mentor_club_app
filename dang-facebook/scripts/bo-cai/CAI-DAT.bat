6ffc0388d34ffd4d106dbadd70cde4690f5d1e29
hangchinaaibusiness-svg <hangchinaaibusiness@gmail.com>
2026-07-04 16:11:00 +0700

    mentor_club_copy
    ok sdgsdgsd

--- dang-facebook/scripts/bo-cai/CAI-DAT.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Cai dat - Dang Reel Facebook
-echo.
-echo   Dang khoi dong trinh cai dat...
-echo.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
